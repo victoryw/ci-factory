bcb0dfa39cb939242c24704c9cc6c4789cd5f404
tianyawy <tianya_wy@hotmail.com>
2018-09-01 17:57:43 +0800

    test with chain

--- src/jobs/monitorCodeBaseJob.groovy
+++ src/jobs/monitorCodeBaseJob.groovy
@@ -1,8 +1,6 @@
 import com.victoryw.jenkins.job.builder.scaffold.MonitorJobBuilder
+import javaposse.jobdsl.dsl.DslFactory
 
 def jobName = 'monitor-code-base'
-def description = new MonitorJobBuilder().
-        jobName('test-with-code').
-        jobDescription(jobName)
-println description.metaClass.getMethods()
-description.build()
+def builder = new MonitorJobBuilder()
+builder.jobName 'test-with-code' jobDescription jobName build (this as DslFactory)
--- src/main/groovy/com/victoryw/jenkins/job/builder/scaffold/MonitorJobBuilder.groovy
+++ src/main/groovy/com/victoryw/jenkins/job/builder/scaffold/MonitorJobBuilder.groovy
@@ -2,6 +2,7 @@ package com.victoryw.jenkins.job.builder.scaffold
 
 import groovy.transform.builder.Builder
 import groovy.transform.builder.SimpleStrategy
+import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.dsl.Job
 
 @Builder(builderStrategy = SimpleStrategy, prefix = '')
@@ -12,8 +13,8 @@ class MonitorJobBuilder {
     String triggers;
     String validateFilesPath;
 
-    Job build() {
-        job(jobName) {
+    def build = { DslFactory dslFactory ->
+        dslFactory.job(jobName) {
             description jobDescription
             scm {
                 git {
