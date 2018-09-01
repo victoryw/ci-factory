4f3d032a399ffd14d146cf670fd2414898e2e97c
tianyawy <tianya_wy@hotmail.com>
2018-09-01 16:40:30 +0800

    reset the example job to could runable, and test the job remove

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -1,21 +1,8 @@
-job = pipelineJob('example-1')
-job.with {
+job('simple-job').with {
     description('')
     displayName('example-1')
 
-    scm {
-        git('https://github.com/victoryw/test-jekinsfile.git')
-    }
-
-    definition {
-        cpsScm {
-            scm {
-                git('https://github.com/victoryw/test-jekinsfile.git')
-            }
-        }
-    }
-
-    triggers {
-        scm('*/2 * * * *')
+    steps {
+        shell('echo "this is simple job"')
     }
 }
