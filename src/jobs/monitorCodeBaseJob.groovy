be9256fe5564a571bd50de73e053a4c3ebce09b8
tianyawy <tianya_wy@hotmail.com>
2018-09-01 18:07:40 +0800

    complete the job

--- src/jobs/monitorCodeBaseJob.groovy
+++ src/jobs/monitorCodeBaseJob.groovy
@@ -2,7 +2,11 @@ import com.victoryw.jenkins.job.builder.scaffold.MonitorJobBuilder
 import javaposse.jobdsl.dsl.DslFactory
 
 def jobName = 'monitor-code-base'
+def codeRepo = 'https://github.com/victoryw/delay-queue.git'
 new MonitorJobBuilder().
         jobName(jobName).
         jobDescription(jobName).
+        gitRemoteUrl(codeRepo).
+        triggers('H/2 * * * *').
+        validateFilesPath('./src/main').
         build(this as DslFactory)
